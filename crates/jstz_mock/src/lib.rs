a3dd73556c312ed812e53a19d35e2db90a649eee
Ryan Tan <excalcabter@gmail.com>
2024-08-01 17:03:24 +0100

    feat(mock): add mock internal message for fa deposi

--- crates/jstz_mock/src/lib.rs
+++ crates/jstz_mock/src/lib.rs
@@ -1,1 +1,49 @@
-pub mod mock;
+use jstz_crypto::hash::Blake2b;
+use tezos_crypto_rs::hash::ContractKt1Hash;
+use tezos_smart_rollup::{
+    michelson::{
+        ticket::{FA2_1Ticket, Ticket},
+        MichelsonBytes, MichelsonNat, MichelsonOption, MichelsonPair,
+    },
+    types::Contract,
+};
+
+pub mod host;
+pub mod message;
+
+pub fn parse_ticket(
+    ticketer: ContractKt1Hash,
+    amount: u32,
+    content: (u32, Option<Vec<u8>>),
+) -> FA2_1Ticket {
+    let ticket_content = MichelsonPair(
+        MichelsonNat::from(content.0),
+        MichelsonOption::<MichelsonBytes>(content.1.clone().map(MichelsonBytes)),
+    );
+
+    Ticket::new(
+        Contract::Originated(ticketer.clone()),
+        ticket_content,
+        amount,
+    )
+    .unwrap()
+}
+
+pub fn account1() -> jstz_crypto::public_key_hash::PublicKeyHash {
+    jstz_crypto::public_key_hash::PublicKeyHash::from_base58(
+        "tz1KqTpEZ7Yob7QbPE4Hy4Wo8fHG8LhKxZSx",
+    )
+    .unwrap()
+}
+
+pub fn account2() -> jstz_crypto::public_key_hash::PublicKeyHash {
+    jstz_crypto::public_key_hash::PublicKeyHash::from_base58(
+        "tz1QcqnzZ8pa6VuE4MSeMjsJkiW94wNrPbgX",
+    )
+    .unwrap()
+}
+
+pub fn ticket_hash1() -> Blake2b {
+    let data = vec![b'0', b'0', b'0'];
+    Blake2b::from(&data)
+}
--- crates/jstz_proto/src/executor/fa_deposit.rs
+++ crates/jstz_proto/src/executor/fa_deposit.rs
@@ -154,7 +154,6 @@ mod test {
 
     use jstz_core::kv::Transaction;
     use jstz_crypto::public_key_hash::PublicKeyHash;
-    use jstz_mock::mock;
     use tezos_smart_rollup_mock::MockHost;
 
     use crate::{
@@ -167,9 +166,9 @@ mod test {
         FaDeposit {
             inbox_id: 34,
             amount: 42,
-            receiver: mock::account2(),
+            receiver: jstz_mock::account2(),
             proxy_smart_function: proxy,
-            ticket_hash: mock::ticket_hash1(),
+            ticket_hash: jstz_mock::ticket_hash1(),
         }
     }
 
@@ -253,7 +252,7 @@ mod test {
         let mut host = MockHost::default();
         host.set_debug_handler(empty());
         let mut tx = Transaction::default();
-        let source = mock::account1();
+        let source = jstz_mock::account1();
         let code = r#"
         export default (request) => {
             const url = new URL(request.url)
@@ -301,7 +300,7 @@ mod test {
         let mut host = MockHost::default();
         host.set_debug_handler(empty());
         let mut tx = Transaction::default();
-        let source = mock::account1();
+        let source = jstz_mock::account1();
         let code = r#"
         export default (request) => {
             const url = new URL(request.url)
@@ -354,7 +353,7 @@ mod test {
         host.set_debug_handler(empty());
         let mut tx = Transaction::default();
         tx.begin();
-        let source = mock::account1();
+        let source = jstz_mock::account1();
         let code = r#"
         export default (request) => {
             const url = new URL(request.url)
